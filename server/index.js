5b5e0468c1f89d1d2ba0a971d813381469cb04f1
铉嘉伟 <xuanjw@infore.com>
2018-01-04 23:08:34 +0800

    通过socket interval 每60s获取一个图片

--- server/index.js
+++ server/index.js
@@ -11,79 +11,21 @@ server.listen(port, function () {
 });
 
 app.use(express.static(path.resolve('./')));
-
-var numUsers = 0;
-
+var i = 0;
 io.on('connection', function (socket) {
     console.log('--Connection--')
-    let videoWith = 0,
-        videoHeight = 0;
-    socket.on('message', function (data) {
-        // socket.broadcast.emit('message', data);
-        console.log(data)
-        videoWith = data.width
-        videoHeight = data.height
-    })
     socket.on('image', function (data) {
-        // console.log('image:' + data)
-        var d1 = +(new Date())
-        var data = data.split('data:image/jpeg;base64,')
-        data = data[1]
-        var bitmap = new Buffer(data, 'base64')
-        var fileName = './static/img/pic' + (+new Date()) + '.jpg'
-        fs.writeFileSync(path.join(fileName), bitmap)
-        var d2 = +(new Date())
-        // console.log('--耗时:' + (d2 - d1))
+        // 后台接收到图片
+        if (i === 0) {
+            console.log(data)
+            i++
+        }
 
-        getFace(fileName)
-    })
-
-
-    function getFace(fileName) {
-        const cv = require('opencv');
-        var d1 = +(new Date())
-        cv.readImage(fileName, function (err, im) {
-            if (err) throw err;
-            if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
-
-            im.detectObject(cv.FACE_CASCADE, {}, function (err, faces) {
-                if (err) throw err;
-
-                // for (var i = 0; i < faces.length; i++) {
-                //     var face = faces[i];
-                //     // im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
-                //     console.log(face)
-                // }
-                if (faces && faces.length > 0) {
-                    var face = faces[0]
-                    d2 = +(new Date())
-                    var frameObj = {
-                        info: {
-                            left: face.x + 'px',
-                            top: face.y + 'px',
-                            width: face.width + 'px',
-                            height: face.height + 'px'
-                        },
-                        isShow: true
-                    }
-                    console.log('--frameObj:', frameObj, '--time:', (d2 - d1))
-                    io.sockets.emit('frame', frameObj)
-                } else {
-                    var frameObj = {
-                        isShow: false,
-                        info: {}
-                    }
-                    io.sockets.emit('frame', frameObj)
-                    // console.log(+(new Date()) + '---none---')
-                }
-
-
-                // im.save('./face-detection.png');
-
-                // console.log('Image saved to ./tmp/face-detection.png' + (d2 - d1));
-            });
-        });
-    }
+    });
 });
 
-io.listen(server);
+var serv_io = io.listen(server);
+
+serv_io.sockets.on('connection', function (socket) {
+    socket.emit('message', {'message': '连接成功'});
+});
