a8411ecc7fa5b51391bf5d2ce4b7921f4e122b24
铉嘉伟 <mifind@sina.com>
2018-01-12 17:04:09 +0800

    现在代码有问题

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ io.on('connection', function (socket) {
         // 后台接收到图片
         if (i === 0) {
             console.log(data)
+            checkPhoto(data)
             i++
         }
 
@@ -29,3 +30,25 @@ var serv_io = io.listen(server);
 serv_io.sockets.on('connection', function (socket) {
     socket.emit('message', {'message': '连接成功'});
 });
+
+
+const checkPhoto = (d) =>{
+    cv.readImage(d, function(err, im){
+        console.log(err)
+        console.log(im)
+        if (err) throw err;
+        if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
+
+        im.detectObject("./data/haarcascade_frontalface_alt.xml", {}, function(err, faces){
+            if (err) throw err;
+
+            for (var i = 0; i < faces.length; i++){
+                var face = faces[i];
+                im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
+            }
+
+            im.save('./tmp/face-detection.png');
+            console.log('Image saved to ./tmp/face-detection.png');
+        });
+    });
+}
